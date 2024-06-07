c8e57c8822c6db0de61875cc52380544392269ee
Alejandrogz1604 <153469692+Alejandrogz1604@users.noreply.github.com>
2024-06-06 21:38:11 -0400

    Add files via upload

---
+++ academic_data_import.sql
@@ -0,0 +1,79 @@
+DROP TABLE IF EXISTS MathSuccess;
+DROP TABLE IF EXISTS PortugueseSuccess;
+
+CREATE TABLE MathSuccess
+(
+	school VARCHAR(50),  
+	sex VARCHAR(25),  
+	age INT,  
+	address VARCHAR(25),  
+	famsize VARCHAR(25),
+ 	Pstatus VARCHAR(25),  
+	Medu INT,  
+	Fedu INT,  
+	Mjob VARCHAR(50),
+ 	Fjob VARCHAR(50),  
+	reason VARCHAR(50),  
+	guardian VARCHAR(50), 
+	traveltime INT,  
+	studytime INT,  
+	failures INT,  
+	schoolsup VARCHAR(25),  
+	famsup VARCHAR(50),  
+	paid VARCHAR(50),  
+	activities VARCHAR(50),  
+	nursery VARCHAR(50),  
+	higher VARCHAR(50),  
+	internet VARCHAR(50),  
+	romantic VARCHAR(50),  
+	famrel INT,  
+	freetime INT,  
+	goout INT,  
+	Dalc INT,  
+	Walc INT,  
+	health INT,  
+	absences INT,  
+	G1 INT,  
+	G2 INT,  
+	G3 INT
+
+);
+
+CREATE TABLE PortugueseSuccess
+(
+	school VARCHAR(50),  
+	sex VARCHAR(25),  
+	age INT,  
+	address VARCHAR(25),  
+	famsize VARCHAR(25),
+ 	Pstatus VARCHAR(25),  
+	Medu INT,  
+	Fedu INT,  
+	Mjob VARCHAR(50),
+ 	Fjob VARCHAR(50),  
+	reason VARCHAR(50),  
+	guardian VARCHAR(50), 
+	traveltime INT,  
+	studytime INT,  
+	failures INT,  
+	schoolsup VARCHAR(25),  
+	famsup VARCHAR(50),  
+	paid VARCHAR(50),  
+	activities VARCHAR(50),  
+	nursery VARCHAR(50),  
+	higher VARCHAR(50),  
+	internet VARCHAR(50),  
+	romantic VARCHAR(50),  
+	famrel INT,  
+	freetime INT,  
+	goout INT,  
+	Dalc INT,  
+	Walc INT,  
+	health INT,  
+	absences INT,  
+	G1 INT,  
+	G2 INT,  
+	G3 INT
+
+);
+
